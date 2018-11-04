35438e5e1c5eebbbaa11fcc3b57c19234abcf61a
Vamsee Krishna Gangapatnam <crvamsee@gmail.com>
2018-11-03 23:27:14 -0500

    TEst

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -2,63 +2,86 @@ var a, b, result;
 
 
 function setvalues() {
-    a = Number(document.getElementById("a").value)
-    b = Number(document.getElementById("b").value);
-   
+    a = Number(document.getElementById("formGroupExampleInput").value)
+    b = Number(document.getElementById("formGroupExampleInput2").value);
+
 
 }
 
+function checkvalidation(){
+    let value = document.querySelector('#formGroupExampleInput') ? document.querySelector('#formGroupExampleInput').value: ''
+    let value1 = document.querySelector('#formGroupExampleInput2') ? document.querySelector('#formGroupExampleInput2').value: ''
+    
+    if(value === ""){
+        document.querySelector('#formGroupExampleInput') ? document.querySelector('#formGroupExampleInput').setCustomValidity("This field cannot be empty"): ''
+        return true
+    }
+    else if(value1 === ""){
+        document.querySelector('#formGroupExampleInput1') ? document.querySelector('#formGroupExampleInput1').setCustomValidity("This field cannot be empty"): ''
+        return true
+    }
+    return false
+}
+
+
 function sum(num1, num2) {
-  
+
     if (num1 || num2) {
         return num1 + num2;
     }
-    setvalues();
+    else if(checkvalidation()){
+       return
+    }
+   setvalues();
     result = a + b;
-          
-    
+
+
     alert("The Sum is equal to " + result);
 }
 
 function Diff(num1, num2) {
-  
 
     if (num1 || num2) {
         return num1 - num2;
-    }
+    }else if(checkvalidation()){
+       return
+   }
     setvalues();
     result = a - b;
-    
+
     alert("The Diff is equal to " + result);
-    
+
 }
 
 function mul(num1, num2) {
-  
-        if (num1 || num2) {
-            return num1 * num2;
-        }
-        setvalues();
-        result = a * b;
-        
-        alert("The multiplication operation is equal to " + result);
-      
+
+    if (num1 || num2) {
+        return num1 * num2;
     }
+    else if(checkvalidation()){
+       return
+   }
+   setvalues();
+    result = a * b;
 
-    function div(num1, num2) {
-      
-              
-            if (num1 || num2) {
-                return num1 / num2;
-            }
-            setvalues();
-            result = a / b;
-            
-            alert("The division operation is equal to " + result);
-        }
+    alert("The multiplication operation is equal to " + result);
 
-        function Reset(a, b) {
-            setvalues();
-            document.getElementById("a").value = "";
-            document.getElementById("b").value = "";
-        }
+}
+
+function div(num1, num2) {
+    if (num1 || num2) {
+        return num1 / num2;
+    }else if(checkvalidation()){
+       return
+   }
+    setvalues();
+    result = a / b;
+
+    alert("The division operation is equal to " + result);
+}
+
+function Reset(a, b) {
+    setvalues();
+    document.getElementById("a").value = "";
+    document.getElementById("b").value = "";
+}
